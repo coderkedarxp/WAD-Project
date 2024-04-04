866a518c45b4a33f3448e4f6700051e5dcea5219
Mihika Saraf <mihikaps003@gmail.com>
2024-04-04 22:14:51 +0530

    login and signup page

--- static/js/main.js
+++ static/js/main.js
@@ -22,3 +22,39 @@ ScrollReveal().reveal(".header__container .btn", {
 document.querySelector(".discover__btn").addEventListener("click", function() {
     window.location.href = "restaurants.html"; // Replace "new-page.html" with the URL of the page you want to redirect to
 });
+
+var a = 1;
+    function pass(){
+        var passwordField = document.getElementById('password');
+        var passIcon = document.getElementById('pass-icon');
+
+        if(a == 1) {
+            passwordField.type = "text";
+            passIcon.classList.remove('fa-eye-slash');
+            passIcon.classList.add('fa-eye');
+            a = 0;
+        } else {
+            passwordField.type = "password";
+            passIcon.classList.remove('fa-eye');
+            passIcon.classList.add('fa-eye-slash');
+            a = 1;
+        }
+    }
+
+    var b = 1;
+    function confirmpass(){
+        var passwordField = document.getElementById('confirmpassword');
+        var passIcon = document.getElementById('confirmpass-icon');
+
+        if(b == 1) {
+            passwordField.type = "text";
+            passIcon.classList.remove('fa-eye-slash');
+            passIcon.classList.add('fa-eye');
+            b = 0;
+        } else {
+            passwordField.type = "password";
+            passIcon.classList.remove('fa-eye');
+            passIcon.classList.add('fa-eye-slash');
+            b = 1;
+        }
+    }
