9bbc64b6b8269c24ea22e0edaf004fdd53e38d33
Mihika Saraf <mihikaps003@gmail.com>
2024-04-08 11:04:51 +0530

    Updated restaurant review pages, restaurant card arrangement

--- static/js/main.js
+++ static/js/main.js
@@ -23,7 +23,7 @@ document.querySelector(".discover__btn").addEventListener("click", function() {
     window.location.href = "restaurants.html"; // Replace "new-page.html" with the URL of the page you want to redirect to
 });
 
-var a = 1;
+var a = 1; //show and unshow password
     function pass(){
         var passwordField = document.getElementById('password');
         var passIcon = document.getElementById('pass-icon');
@@ -41,7 +41,7 @@ var a = 1;
         }
     }
 
-    var b = 1;
+    var b = 1; //show and unshow password
     function confirmpass(){
         var passwordField = document.getElementById('confirmpassword');
         var passIcon = document.getElementById('confirmpass-icon');
@@ -58,3 +58,36 @@ var a = 1;
             b = 1;
         }
     }
+
+    var c = 0; // Initial state
+function likeOrNone() {
+    var likeIcon = document.getElementById('like-review');
+    // Toggle the 'fa-solid' class based on the current state
+    if (c === 0) {
+        likeIcon.classList.add('fa-solid');
+        c = 1;
+    } else {
+        likeIcon.classList.remove('fa-solid');
+        likeIcon.classList.add('fa-regular')
+        c = 0;
+    }
+}
+
+var d = 0;
+function unlikeOrNone() {
+    var unlikeIcon = document.getElementById('unlike-review');
+    if(d === 0) {
+        unlikeIcon.classList.add('fa-solid');
+        d = 1;
+    } else {
+        unlikeIcon.classList.remove('fa-solid');
+        unlikeIcon.classList.add('fa-regular');
+        d = 0;
+    }
+}
+
+
+var myCarousel = document.getElementById('carouselExampleIndicators');
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000 // Adjust the interval as needed
+});
